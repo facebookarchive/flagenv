765fb52d2765c01d9fc38188a15bbdf7559cd760
Seth Ammons <seth.ammons@sendgrid.com>
2016-04-23 08:44:20 -0700

    provide example usage
    I did not run godoc on this change, but it should be legit. The current GoDoc does not provide any usage examples.

--- flagenv_test.go
+++ flagenv_test.go
@@ -14,6 +14,17 @@ import (
 
 func named(t, v string) string { return strings.ToUpper(t + v) }
 
+func ExampleParse(){
+	var raz string
+	flag.StringVar(&raz, "raz-value", "bar", "set the raz")
+	
+	// override default flag value with value found in MY_RAZ_VALUE
+	flagenv.Prefix = "my_"
+	flagenv.Parse()
+	// override value found in MY_RAZ_VALUE with command line flag value -raz-value=foo
+	flag.Parse()
+}
+
 func TestNothingToDo(t *testing.T) {
 	const name = "TestNothingToDo"
 	s := flag.NewFlagSet(name, flag.PanicOnError)
